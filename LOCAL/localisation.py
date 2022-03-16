9d298f9c8523d910aedb808efba89e756c068333
Devill Don <53294521+devillD@users.noreply.github.com>
2022-03-16 22:17:36 +0600

    Update localisation.py

--- LOCAL/localisation.py
+++ LOCAL/localisation.py
@@ -7,11 +7,11 @@ FORCE_SUB_TEXT = "In order to use this bot, you've to join my parent channel."
 
 CHANNEL_LINK = "https://t.me/DroneBots"
 
-SUPPORT_LINK = "https://t.me/TeamDrone"
+SUPPORT_LINK = "https://t.me/unkusr"
 
-info_text = "This bot is developed by @MaheshChauhan\n\nWritten in python library TELETHON.\n\nBot by : @DroneBots\nSupport : @TeamDrone\n\nV1.4"   
+info_text = "This bot is developed by @MaheshChauhan"   
 
-help_text = """**v1.4**
+help_text = """
 
 •`Encode` - encode your video into different lib format or resolution
 
@@ -31,15 +31,14 @@ source_text = "**Deploy your own bot**"
 
 DEV = "https://t.me/MaheshChauhan"
 
-spam_notice = "This bot is hosted on heroku, and hence can just run one process at a time.Spamming the bot or encoding adult videos will lead you to a ban."
+spam_notice = "This bot is hosted on a vps with low resource :)"
 
 JPG = "LOCAL/video_convertor.jpg"
 
-JPG0 = 'https://telegra.ph/file/d98c559b56ef884ef3bad.jpg'
+JPG0 = 'https://telegra.ph/file/30c01ece5066de4776119.gif'
 
 JPG2 = "LOCAL/20211215_165751.jpg"
 
 JPG3 = "LOCAL/PicsArt_12-16-08.57.15.jpg"
 
 JPG4 = "LOCAL/20211219_000258.jpg"
-
